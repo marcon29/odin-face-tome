fef70446632b117587c7f43368ddbc252bb51d10
marcon29 <online@markstabler.com>
2022-03-06 01:17:02 -0700

    built out main layout and static#about page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :logged_in?
+
+    # this is just to get some variables working for main layout until models are set up
+    def logged_in?
+        true
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # change to this once built
+  # root 'posts#index'
+  root 'static#about'  
+
+  get 'about', to: 'static#about'
+  
 end

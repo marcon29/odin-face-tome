474d0d6e6a5f9c010180e665575c1328956ee8ff
marcon29 <online@markstabler.com>
2022-03-16 23:26:35 -0600

    Post: built basic CRUD and tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,19 +6,27 @@ class Post < ApplicationRecord
     # attrs: :content, :user_id
 
     # all attrs are required
-        # if content is blank, simply return to page - no error message
     # only content can be updated, not user
+    validates :content, presence: { message: "You must provide some content to post." }
+    validates :user_id, presence: { message: "You must provide a user." }
+    validate :check_user_updating
+    before_validation :format_content
+    
     # only signed_in user can view
     # only can CRUD only if current_user's posts
 
-    # before_validation :format_content
+    
 
 
     # ################ helpers (callbacks & control flow)  ####################
     
     
     # ################ helpers (instantiation & validation)  ####################
-    
+    def check_user_updating
+        if self.persisted?
+            errors.add(:user_id, "Can't change the user of a post.") if self.user_id_changed? 
+        end
+    end
     
     # ################ helpers (nested or associated models)  ####################
         # ######## working with User model
@@ -44,8 +52,10 @@ class Post < ApplicationRecord
     
     
     # ################ helpers (data control)  ####################
-        # can remove beginning and trailing white space
-            # def format_content
-            # end
+    
+    # can remove beginning and trailing white space
+    def format_content
+        self.content = self.content.strip
+    end
     
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -14,9 +14,9 @@ RSpec.describe Post, type: :model do
   # exact duplicate of test_all
   # use as whole for testing unique values
   # use for testing specific atttrs (bad inclusion, bad format, helpers, etc.) - change in test itself
-  let(:duplicate) {
-    {content: "This is content for a post. It's great.", user_id: 1}
-  }
+  # let(:duplicate) {
+  #   {content: "This is content for a post. It's great.", user_id: 1}
+  # }
 
   # take test_all and remove any non-required attrs and auto-assign (not auto_format) attrs, all should be formatted correctly
   # let(:test_req) {
@@ -34,38 +34,112 @@ RSpec.describe Post, type: :model do
   }
 
   # ###################################################################
+  # define custom error messages
+  # ###################################################################
+  let(:missing_content_message)  {"You must provide some content to post."}
+  let(:missing_user_message) {"You must provide a user."}
+  let(:update_user_message) {"Can't change the user of a post."}
+  
+
+
+  # ###################################################################
   # define tests
   # ###################################################################
   
   # object creation and validation tests #######################################
   describe "model creates and updates only valid instances" do
-    # before(:each) do
-    #     # insert some helper method here if need to
-    # end
+    before(:all) do
+      user1 = User.create(first_name: "Joe", last_name: "Schmo", username: "jschmo", email: "jschmo@example.com", password: "tester")
+    end
     
     describe "valid when " do
-      it "given all required and unrequired valid attributes"
+      it "given all required and unrequired valid attributes" do
         # test w/ test_all
+        expect(User.all.count).to eq(1)
+        expect(Post.all.count).to eq(0)
+        
+        test_post = Post.create(test_all)
+        expect(test_post).to be_valid
+        expect(Post.all.count).to eq(1)
+        
+        expect(test_post.content).to eq(test_all[:content])
+        expect(test_post.user_id).to eq(test_all[:user_id])
+      end
+        
 
-      it "updating all attributes with valid values"
+      it "updating all attributes with valid values" do
         # test w/ update
+        expect(User.all.count).to eq(1)
+        expect(Post.all.count).to eq(0)
+        
+        test_post = Post.create(test_all)
+        expect(test_post).to be_valid
+        expect(Post.all.count).to eq(1)
+
+        # update all attrs, check all have new values for same instance
+        test_post.update(update)
+
+        expect(test_post).to be_valid
+        expect(test_post.content).to eq(update[:content])
+        expect(test_post.user_id).to eq(test_all[:user_id])
+      end
 
     end
 
     describe "invalid and has correct error message when" do
-      it "required attributes are missing"
+      it "required attributes are missing" do
         # test w/ blank
+        expect(User.all.count).to eq(1)
+        expect(Post.all.count).to eq(0)
 
-      it "tries to update request sender or receiver"
+        test_post = Post.create(blank)
+
+        expect(test_post).to be_invalid
+        expect(Post.all.count).to eq(0)
+
+        # add tests as needed
+        expect(test_post.errors.messages[:content]).to include(missing_content_message)
+        expect(test_post.errors.messages[:user_id]).to include(missing_user_message)
+      end
+
+      it "tries to update user" do
         # grab this one from Friend tests
+        user2 = User.create(first_name: "Jack", last_name: "Hill", username: "jhill", email: "jhill@example.com", password: "tester")
+        expect(User.all.count).to eq(2)
+        expect(Post.all.count).to eq(0)
+
+        test_post = Post.create(test_all)
+        expect(test_post).to be_valid
+        expect(Post.all.count).to eq(1)
+
+        test_post.update(user_id: 2)
+        expect(test_post).to be_invalid
+        expect(test_post.content).to eq(test_all[:content])
+        expect(test_post.errors.messages[:user_id]).to include(update_user_message)
+      end
 
     end
   end
 
   # helper method tests ########################################################
   describe "all helper methods work correctly:" do
-    it "can remove beginning and trailing white space"
-      # provide custom test value
+    it "can remove beginning and trailing white space" do
+      user = User.first
+      expect(User.all.count).to eq(1)
+      expect(Post.all.count).to eq(0)
+
+      bad_content = "   this has white space before, in the     middle, and after.   "
+
+      # format_content should clean this up before validationg using .strip
+      
+      # test_post = Post.create(content: bad_content, user: user)
+      test_post = Post.create(content: bad_content, user_id: user.id)
+      
+      expect(test_post).to be_valid
+      expect(Post.all.count).to eq(1)
+      expect(test_post.content).to eq("this has white space before, in the     middle, and after.")
+      expect(test_post.user_id).to eq(user.id)
+    end
 
   end
 

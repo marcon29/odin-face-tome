d19c3544e6dffdd515887f5d886657efe2b90747
marcon29 <online@markstabler.com>
2022-03-17 03:15:23 -0600

    started Post show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,13 @@
 class PostsController < ApplicationController
-  
+  before_action :authenticate_user!
+
   # all posts (filtered by user/users) - each post shows comments (limit 3)
   def index
   end
 
   # one post with all comments
   def show
+    @post = Post.find(params[:id])
   end
 
   # not needed (partial only in feed)?

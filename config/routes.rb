1bb1d5429a31928ed888797eea3b7fd889d57fa4
marcon29 <online@markstabler.com>
2022-03-18 09:23:52 -0600

    generated Comment resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   
+  
+  
   # uses letter_opener gem to test email sending without sending emails
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   
+  root 'posts#index'  
   
+  resources :comments, only: [:create, :edit, :update, :destroy] 
   
-  resources :posts
-
-
+  resources :posts, only: [:index, :show, :create, :edit, :update, :destroy]
   
-
   devise_for :users, controllers: {registrations: "registrations", omniauth_callbacks: "omniauth_callbacks"}
   
   resources :users, only: [:index, :show] do
@@ -18,24 +19,18 @@ Rails.application.routes.draw do
       patch :update_profile_image
       delete :delete_profile_image
     end
-  end
-      
+  end 
+  
   resources :friends, only: [:index, :create, :update, :destroy] do
     collection do
       get "requests"
     end
   end
 
-  # resources :friends do
-  #   collection do
-  #     get "requests"
-  #   end
-  # end
-  
   get 'about', to: 'static#about'
 
-  # change to this once built
-  root 'posts#index'
-  # root 'static#about'
+  
+  
+  
   
 end

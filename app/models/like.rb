5fb412868f9419c2289f5a8f2199e68eb898d162
marcon29 <online@markstabler.com>
2022-03-19 09:46:26 -0600

    planned and stubbed out Like model and tests

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,30 @@
 class Like < ApplicationRecord
+    
+    # has_many :users
+    # has_many :posts
+
+    # attrs: :user_id, :post_id
+
+    # all attrs are required
+    # can't be updated, only created and destroyed
+    # only signed_in user can CRUD (or CD)
+    # can CRUD only if current_user's likes
+
+
+
+    # ################ helpers (callbacks & control flow)  ####################
+    
+    
+    # ################ helpers (instantiation & validation)  ####################
+    
+    
+    # ################ helpers (nested or associated models)  ####################
+        # ######## working with User model
+        # ######## working with Post model
+        # ######## working with Like model
+    
+    
+    # ################ helpers (data control)  ####################
+
+
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # ###################################################################
+  # define main test object attrs
+  # ###################################################################
+  let(:test_all) {
+    {user_id: 1, post_id: 1}
+  }
+
+  # ###################################################################
+  # define standard create/update attr variations
+  # ###################################################################
+  # exact duplicate of test_all
+  # use as whole for testing unique values
+  # use for testing specific atttrs (bad inclusion, bad format, helpers, etc.) - change in test itself
+  # let(:duplicate) {
+  #   {content: "This is content for a post. It's great.", user_id: 1}
+  # }
+
+  # take test_all and remove any non-required attrs and auto-assign (not auto_format) attrs, all should be formatted correctly
+  # let(:test_req) {
+  #   {content: "This is content for a post. It's great."}
+  # }
+
+  # start w/ test_all, change all values, make any auto-assign blank (don't delete), delete any attrs with DB defaults
+  # let(:update) {
+  #   {content: "This is a different comment."}
+  # }
+  
+  # every attr blank
+  let(:blank) {
+    {user_id: "", post_id: ""}
+  }
+
+  # ###################################################################
+  # define custom error messages
+  # ###################################################################
+  let(:missing_user_message) {"You must provide a user."}
+  let(:missing_post_message) {"You must provide a post."}
+  let(:update_user_message) {"Can't change the user of a comment."}
+  let(:update_post_message) {"Can't change the post of a comment."}
+
+  # ###################################################################
+  # define tests
+  # ###################################################################
+  before(:all) do
+    user1 = User.create(first_name: "Joe", last_name: "Schmo", username: "jschmo", email: "jschmo@example.com", password: "tester")
+    # user2 = User.create(first_name: "Jack", last_name: "Hill", username: "jhill", email: "jhill@example.com", password: "tester")
+    # user3 = User.create(first_name: "Jane", last_name: "Doe", username: "janedoe", email: "janedoe@example.com", password: "tester")
+    # user4 = User.create(first_name: "Jill", last_name: "Hill", username: "jillhill", email: "jillhill@example.com", password: "tester")
+    # user5 = User.create(first_name: "John", last_name: "Doe", username: "johndoe", email: "johndoe@example.com", password: "tester")
+
+    post1 = Post.create(content: "This is post1; it's from user 1.", user_id: 1)
+    post2 = Post.create(content: "This is post2; it's from user 1.", user_id: 1)
+    # post3 = Post.create(content: "This is post3; it's from user 2.", user_id: 2)
+  end
+
+  describe "model creates and updates only valid instances" do
+    describe "valid when " do
+      it "given all required and unrequired valid attributes" do
+      end
+    end
+    
+    describe "invalid and has correct error message when" do
+      it "required attributes are missing" do
+      end
+
+      it "tries to update user or post" do
+      end
+    end
+  end
+  
+  describe "instances are properly associated to User and Post models" do
+    it "can find the user that created it and the post it's for" do
+    end
+
+    it "can collect all likes for a specific post" do
+    end
+
+    it "can collect all likes from a specific user"
+  end
+
+
 end
